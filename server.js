94e3d6db4fedf7accada6515932e7867c0bde2ea
Samuel Chávez <schavezfuentes@gmail.com>
2021-02-10 12:30:02 -0600

    Cambio en el microservicio

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log('HOLA SAMUEL!');
+    console.log('QUE BUENO QUE ESTO YA ESTA INTEGRADO A TRAVIS!');
     await sleep(1000);
   }
 }
